b4e63a375c9e30e3ab35e91aa55fefd725633440
kymkyj <ymyj33@naver.com>
2021-04-25 15:47:03 +0900

    Initial commit

---
+++ src/main/java/com/kym/boot/jpashop/Hello.java
@@ -0,0 +1,10 @@
+package com.kym.boot.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+}
---
+++ src/test/java/com/kym/boot/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.kym.boot.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    // spring 에서 제공하는 트랜잭션을 사용하는게 좋다 부가옵션으로 쓸수있는것이 많음
+    // 트랜잭션이 테스트쪽에 있으면 끝나고나서 롤백해버린다.
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
+
+    }
+}
